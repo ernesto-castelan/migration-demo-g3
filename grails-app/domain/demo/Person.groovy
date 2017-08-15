b2ae58322f784e8c51d91f7bce8e608e4d3754bf
Ernesto Castelan <ernesto.castelan@vincoorbis.com>
2017-08-15 14:41:28 -0500

    Adds address fields to Person domain

--- grails-app/domain/demo/Person.groovy
+++ grails-app/domain/demo/Person.groovy
@@ -3,10 +3,16 @@ package demo
 class Person {
     String name
     Integer age
+    String streetName
+    String city
+    String zipCode
 
     static constraints = {
         name blank:false, maxSize:100
         age  nullable:true, min:0
+        streetName  nullable:true
+        city        nullable:true
+        zipCode     nullable:true
     }
 
     static mapping = {
